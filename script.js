26b4e33ead20e9cb65642043bdfaabb8fd92ad31
roopesh04 <arvapalli12345@gmail.com>
2023-09-16 21:02:05 +0530

    added bootstrap to functionality

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Handle login form submission
-import "./config.json"
-console.log("this is working")
+const response = await fetch('./config.json');
+const jsonData = await response.json();
+const apiEndpoint= jsonData["apiEndpoint"]
 
 document.getElementById("loginForm").addEventListener("submit",async function (e) {
     e.preventDefault();
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    const apiEndPoint="http://127.0.0.1:8080/api/v1/login"
-
-    const loginResult = await fetch(apiEndPoint, {
+    const loginResult = await fetch(apiEndpoint+"/login", {
         method:"POST",
         headers:{
             "Content-Type":"application/json"
@@ -24,17 +23,57 @@ document.getElementById("loginForm").addEventListener("submit",async function (e
         document.getElementById("profile").style.display = "block";
         document.getElementById("leave").style.display = "block";
         document.getElementById("login").style.display = "none";
+        document.getElementById("alertContainer").style.display = "none";
         const jsonResponse=await loginResult.json()
 
         localStorage.setItem("accessToken",jsonResponse["accessToken"])
+
+        let userData=await fetch(apiEndpoint+"/employee/1",{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        userData=await userData.json()
+
+        document.getElementById("firstName").innerText=userData["firstName"]
+        document.getElementById("lastName").innerText=userData["lastName"]
+        document.getElementById("dob").innerText=userData["dob"]
+        document.getElementById("sex").innerText=userData["sex"]
+
+
     }else{
-        alert("Please provide valid Emp ID aand Password")
+
+        document.getElementById('alertContainer').innerHTML = `
+          <div class="alert alert-danger" role="alert">
+            Please provide valid Emp ID and Password
+          </div>`
     }
 
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const token = localStorage.getItem("accessToken");
+document.addEventListener("DOMContentLoaded",async function () {
+    var token = localStorage.getItem("accessToken");
+    debugger
+    const apiEndPoint=apiEndpoint+"/login/validate-token"
+    debugger
+    console.log("This is working")
+
+    const loginResult = await fetch(apiEndPoint, {
+        method:"GET",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":token
+        }
+    });
+    debugger
+    console.log("LOgin result "+loginResult.status)
+    if(loginResult.status!=200) {
+        localStorage.removeItem("accessToken")
+        token=null
+    }
+
+
     if (token) {
         document.getElementById("profile").style.display = "block";
         document.getElementById("leave").style.display = "block";
